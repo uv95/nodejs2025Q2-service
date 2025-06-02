8b5a0cad3a3b0af79258cf64a4f1402deb6dd5c5
Uliana <ulianav95v@gmail.com>
2025-06-02 13:55:51 -0600

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { join } from 'node:path';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'node:fs/promises';
+import * as YAML from 'yaml';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
+
+  const yamlPath = join(__dirname, '..', 'doc', 'api.yaml');
+  const content = await fs.readFile(yamlPath, 'utf8');
+  const doc = YAML.parse(content);
+
+  SwaggerModule.setup('doc', app, doc);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +23,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT);
+  await app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`API is available at http://localhost:${PORT}/doc`);
+  });
 }
 bootstrap();
