5f3279c81462533afd5d926d53b20fcb2e4e8c07
Uliana <ulianav95v@gmail.com>
2025-06-03 17:41:21 -0600

    feat: add prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TrackModule } from './modules/track/track.module';
 import { AlbumModule } from './modules/album/album.module';
 import { UserModule } from './modules/user/user.module';
 import { FavoritesModule } from './modules/favorites/favorites.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { FavoritesModule } from './modules/favorites/favorites.module';
     TrackModule,
     AlbumModule,
     FavoritesModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
